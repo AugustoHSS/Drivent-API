5a9108841adb00c87927b1be394345802f44cb3e
Rui Neto <ruineto11@gmail.com>
2022-06-16 01:09:45 -0300

    chore: deploy

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -5,12 +5,7 @@ dotenv.config();
 
 export let redis: RedisClientType;
 export async function connectRedis(): Promise<void> {
-  const url =
-    process.env.NODE_ENV === "development" ||
-    process.env.NODE_ENV === "test" ||
-    process.env.NODE_ENV === "local"
-      ? process.env.REDIS_URL
-      : "redis://default:123456@drivent-postgres-production-redis:6379";
+  const url = "redis://default:123456@drivent-postgres-production-redis:6379";
 
   redis = createClient({ url });
 
