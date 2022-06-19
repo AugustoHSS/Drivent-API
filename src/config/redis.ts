973ee73ab98dddeadaf021ef41f0efbe4fb8fae1
Rui Neto <ruineto11@gmail.com>
2022-06-19 01:15:46 -0300

    chore: dockerfile and docker-compose

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -5,7 +5,12 @@ dotenv.config();
 
 export let redis: RedisClientType;
 export async function connectRedis(): Promise<void> {
-  const url = process.env.REDIS_URL;
+  const url = "redis://default:123456@drivent-redis-production:6379";
+  /*     process.env.NODE_ENV === "development" ||
+    process.env.NODE_ENV === "test" ||
+    process.env.NODE_ENV === "local"
+      ? process.env.REDIS_URL
+      : "redis://default:123456@drivent-redis-production:6379"; */
 
   redis = createClient({ url });
 
